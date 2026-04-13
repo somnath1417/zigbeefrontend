76692097e8d04a894716119bb575473293234fe9
somnath1417 <patilsomnath1417@gmail.com>
2026-04-13 15:58:06 +0530

    update tetsing URL

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
 
 export const socket = io(SOCKET_URL, {
-  transports: ["websocket"],
+  transports: ["websocket", "polling"],
   autoConnect: true,
 });
 
